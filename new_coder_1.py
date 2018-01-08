14e7fd787f1242bfd56f0c0003e7021f7b29dd99
张明哲 <1084904209@qq.com>
2018-01-08 10:40:50 +0800

    电话号的最终解

--- new_coder_1.py
+++ new_coder_1.py
@@ -1,71 +1,23 @@
 def get_number(str_):
     result = []
-    str_ = list(str_)
-    i = iter(str_)
-    print(next(i))
-    for s in str_:
-        if "Z" == s:
-            result.append(2)
-            str_.remove("Z")
-            str_.remove("E")
-            str_.remove("R")
-            str_.remove("O")
-        elif "W" == s:
-            result.append(4)
-            str_.remove("T")
-            str_.remove("W")
-            str_.remove("O")
-        elif "U" == s:
-            result.append(6)
-            str_.remove("F")
-            str_.remove("O")
-            str_.remove("U")
-            str_.remove("R")
-        elif "X" == s:
-            result.append(8)
-            str_.remove("S")
-            str_.remove("I")
-            str_.remove("X")
-        elif "G" == s:
-            result.append(0)
-            str_.remove("E")
-            str_.remove("I")
-            str_.remove("G")
-            str_.remove("H")
-            str_.remove("T")
-    print(str_)
-    for s in str_:
-        if 'O' == s:
-            result.append(3)
-            str_.remove("O")
-            str_.remove("N")
-            str_.remove("E")
-        elif "T" == s:
-            result.append(5)
-            str_.remove("T")
-            str_.remove("H")
-            str_.remove("R")
-            str_.remove("E")
-            str_.remove("E")
-        elif "F" == s:
-            result.append(7)
-            str_.remove("F")
-            str_.remove("I")
-            str_.remove("V")
-            str_.remove("E")
-        elif "S" == s:
-            result.append(9)
-            str_.remove("S")
-            str_.remove("E")
-            str_.remove("V")
-            str_.remove("N")
-            str_.remove("E")
-    print(str_)
-    c = int(len(str_) / 4)
-    for i in range(c):
-        result.append(1)
-    print(result)
-    # return "".join(result)
+    count_0 = str_.count("Z")
+    count_2 = str_.count("W")
+    count_4 = str_.count("U")
+    count_6 = str_.count("X")
+    count_8 = str_.count("G")
+    count_1 = str_.count("O") - count_0 - count_2 - count_4
+    count_7 = str_.count("S") - count_6
+    result += ["0"] * count_8
+    result += ["1"] * int((str_.count("N") - count_1 - count_7) / 2)
+    result += ["2"] * count_0
+    result += ["3"] * count_1
+    result += ["4"] * count_2
+    result += ["5"] * (str_.count("T") - count_2 - count_8)
+    result += ["6"] * count_4
+    result += ["7"] * (str_.count("F") - count_4)
+    result += ["8"] * count_6
+    result += ["9"] * count_7
+    return "".join(result)
 
 
 def main():
@@ -83,5 +35,5 @@ def test():
 
 
 if __name__ == '__main__':
-    # main()
-    test()
+    main()
+    # test()
